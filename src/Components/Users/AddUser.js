6182524249350b27ac9f305036ed9717cba1c0ce
Hassan Essam <58363102+DevDerpi@users.noreply.github.com>
2021-12-19 23:32:29 +0200

    Add files via upload

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
-  const [userName, setUserName] = useState("");
-  const [age, setAge] = useState("");
+  // const [userName, setUserName] = useState("");
+  // const [age, setAge] = useState("");
+  const userNameRef = useRef();
+  const userAgeRef = useRef();
   // const [user, setUser] = useState({});
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const usernameChangeHandler = (event) => {
-    setUserName(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setAge(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setUserName(event.target.value);
+  // };
+  // const ageChangeHandler = (event) => {
+  //   setAge(event.target.value);
+  // };
   const submitChangeHandler = (event) => {
     event.preventDefault();
-    if (userName && age>0) {
+    const userName = userNameRef.current.value;
+    const age = userAgeRef.current.value;
+    if (userName && age > 0) {
       const user = {
         userName: userName,
         age: +age,
@@ -27,15 +31,21 @@ const AddUser = (props) => {
       };
       console.log(user);
       props.onSubmit(user);
-      setUserName("");
-      setAge("");
+      userNameRef.current.value = "";
+      userAgeRef.current.value = "";
     } else {
       if (!userName || !age) {
         setIsError(true);
-        setErrorMessage({title: "Invalid Input ",body : "Please Enter a Valid name and age (non-empty values)."});
-      } else if (age < 1) {
+        setErrorMessage({
+          title: "Invalid Input ",
+          body: "Please Enter a Valid name and age (non-empty values).",
+        });
+      } else if (+age < 1) {
         setIsError(true);
-        setErrorMessage({title: "Invalid Age",body:"Please Enter a Valid Age (greater than 1 year)."});
+        setErrorMessage({
+          title: "Invalid Age",
+          body: "Please Enter a Valid Age (greater than 1 year).",
+        });
       }
     }
   };
@@ -50,9 +60,9 @@ const AddUser = (props) => {
       )}
       <form className={styles.input} onSubmit={submitChangeHandler}>
         <label>Username</label>
-        <input onChange={usernameChangeHandler} value={userName} type="text" />
+        <input ref={userNameRef} type="text" />
         <label>Age (Years)</label>
-        <input onChange={ageChangeHandler} value={age} type="number" />
+        <input ref={userAgeRef} type="number" />
         <Button>Add User</Button>
       </form>
     </Card>
